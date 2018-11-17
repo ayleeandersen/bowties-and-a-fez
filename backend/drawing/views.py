027cb50826a9b284f6147cb7d6b251362bfd00a2
camilne <cameronm213@gmail.com>
2018-11-16 20:02:04 -0700

    Add fake response

--- backend/drawing/views.py
+++ backend/drawing/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+# from .stroke_command import StrokeCommand
+# from .point import Point
+
+def room(request, room_number):
+    # command = StrokeCommand(Point(0, 0), Point(300, 200))
+    # return JsonResponse(command, safe=False)
+    return JsonResponse({'commandType':'stroke', 'start':{'x':'120.7','y':'150'}, 'end':{'x':'1','y':'25'}})
