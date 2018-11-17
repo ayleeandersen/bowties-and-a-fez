285185d53c0dc36a85ac8cda88a5c51298e19cc8
Aylee <ayleeandersen@gmail.com>
2018-11-17 00:22:49 -0700

    Added Canvas component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CommandFactoryComponent } from './command-factory/command-factory.component';
+import { StrokeComponent } from './stroke/stroke.component';
+import { CanvasComponent } from './canvas/canvas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommandFactoryComponent
+    CommandFactoryComponent,
+    StrokeComponent,
+    CanvasComponent
   ],
   imports: [
     BrowserModule,
