675987475227e702d65e27f5bc6b8ff82ae9ecd8
Aylee <ayleeandersen@gmail.com>
2018-11-16 23:50:24 -0700

    Draws a line

--- frontend/src/app/command-factory/command-factory.component.ts
+++ frontend/src/app/command-factory/command-factory.component.ts
@@ -49,6 +49,7 @@ export class CommandFactoryComponent implements OnInit {
       this.canvas.beginPath();
       this.canvas.moveTo(this.command.start.x, this.command.start.y);
       this.canvas.lineTo(this.command.end.x, this.command.end.y);
+      this.canvas.stroke();
     } else if (this.command.commandType === "fill") {
       // call draw in fill class
     }
